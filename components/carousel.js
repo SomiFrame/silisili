204556070688cfb55bbd8dc84cafacd51627e206
frame <frame@moonjoy.net>
2018-05-23 17:57:58 +0700

    update

--- components/carousel.js
+++ components/carousel.js
@@ -1,22 +1,25 @@
 import { Component } from 'react'
-import { Carousel,Button,Icon } from 'antd'
+import { Carousel, Button, Icon } from 'antd'
+import Slider from "react-slick"
 import "./carousel.scss"
 class CustomCarousel extends Component {
     render() {
+        const setting = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            className: "custom-slider"
+        }
         return (
             <div className="carousel-container">
-                <Button className="carousel-left" onClick={()=>console.log(this)}>
-                    <Icon type="left" />
-                </Button>
-                <Button className="carousel-right">
-                    <Icon type="right" />
-                </Button>
-                <Carousel autoplay>
+                <Slider autoplay>
                     <div><h3>1</h3></div>
                     <div><h3>2</h3></div>
                     <div><h3>3</h3></div>
                     <div><h3>4</h3></div>
-                </Carousel>
+                </Slider>
             </div>
         )
     }
--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,8 @@ export default class MyDocument extends Document {
           <style>{`body { margin: 0 } /* custom! */`}</style>
           <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/antd/3.2.0/antd.min.css' />
           <link rel="stylesheet" href="/_next/static/style.css" />
+          <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+          <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
         </Head>
         <body className="custom_class">
           <Main />
