d262623c0162c5e5afc7d3e93cbeb27e32a96d53
frame <frame@moonjoy.net>
2018-06-04 17:33:53 +0700

    update

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,7 @@
 import { Component, Children } from 'react'
 import { Layout, Menu, Icon } from 'antd'
 import CustomHeader from './header'
+import "./layout.scss"
 const { Header, Content, Footer, Slider } = Layout
 class layout extends Component {
     constructor(props) {
@@ -10,7 +11,7 @@ class layout extends Component {
         return (
             <Layout className="page-layout-root" style={{ minHeight: '100vh' }}>
                 <CustomHeader />
-                <div className="content">
+                <div className="page-container">
                     {this.props.children}
                 </div>
             </Layout>
--- pages/index.js
+++ pages/index.js
@@ -51,33 +51,63 @@ class Page extends Component {
         }
         return (
             <Layout>
-                <img className="index-page-background" src="/static/imgs/page-background-luo.jpg" />
-                <section id="section-1">
-                    <div style={{ width: 700, height: 400, margin: '40px auto' }}>
-                        <Carousel dataList={this.props.recommends} />
-                    </div>
-                </section>
-                <section id="section-dayly-update">
-                    <Card title={"今日更新"} bordered={false}>
-                        <Row gutter={16}>
-                            {
-                                [1, 2, 3, 4, 5, 6].map(() =>
-                                    <Col md={8} sm={12} xs={24} lg={4}>
-                                        <Card
-                                            hoverable
-                                            style={{ width: "100%" }}
-                                            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} >
-                                            <Meta
-                                                title="Europe Street beat"
-                                                description="www.instagram.com"
-                                            />
-                                        </Card>
-                                    </Col>
-                                )
-                            }
-                        </Row>
-                    </Card>
-                </section>
+                <div className="content-container">
+                    <section id="section-1">
+                        <div style={{ width: 700, height: 400, margin: '40px auto' }}>
+                            <Carousel dataList={this.props.recommends} />
+                        </div>
+                    </section>
+                    <section id="section-dayly-update">
+                        <Card title={"今日更新"} bordered={false}>
+                            <Row gutter={16}>
+                                {
+                                    [1, 2, 3, 4, 5, 6].map((val, index) =>
+                                        <Col key={index} md={8} sm={12} xs={24} lg={4}>
+                                            <Card
+                                                hoverable
+                                                style={{ width: "100%" }}
+                                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} >
+                                                <Meta
+                                                    title="Europe Street beat"
+                                                    description="www.instagram.com"
+                                                />
+                                            </Card>
+                                        </Col>
+                                    )
+                                }
+                            </Row>
+                        </Card>
+                    </section>
+                    <section className="parallax-group index-parallax">
+                        <div className="parallax-layer parallax-layer_back">
+                        </div>
+                        <div className="parallax-layer parallax-layer_base">
+                                <h1>asdlfuqoiwer</h1>
+                        </div>
+                    </section>
+                    <section id="section-dayly-update">
+                        <Card title={"本周热门"} bordered={false}>
+                            <Row gutter={16}>
+                                {
+                                    [1, 2, 3, 4, 5, 6].map((val, index) =>
+                                        <Col key={index} md={8} sm={12} xs={24} lg={4}>
+                                            <Card
+                                                hoverable
+                                                style={{ width: "100%" }}
+                                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} >
+                                                <Meta
+                                                    title="Europe Street beat"
+                                                    description="www.instagram.com"
+                                                />
+                                            </Card>
+                                        </Col>
+                                    )
+                                }
+                            </Row>
+                        </Card>
+                    </section>
+
+                </div>
             </Layout>
         )
 
